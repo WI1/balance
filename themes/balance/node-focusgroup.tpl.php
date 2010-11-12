5b19da3bdf79101ace5eb5a0dc8706f7b761f326
dominikboehler <dominikboehler>
2010-11-12 12:57:19 +0000

    added viewfield and focusgroup speaker

--- themes/balance/node-focusgroup.tpl.php
+++ themes/balance/node-focusgroup.tpl.php
@@ -4,12 +4,14 @@
 <?php elseif(arg(2) == 'info'): ?>
 <?php print $node->content['og_mission']['#value'] ?>
 <dl>
-  <dt>Betreuung DLR</br>
+ <dt>Fokusgruppensprecher</dt>
+ <dd><?php print phptemplate_business_card($node->field_focusgroup_speaker[0]['uid']); ?></dd>
+ <dt>Betreuung DLR</br>
 <?php print $node->field_contactdlrpicture[0]['view'] ?></dt>
  <dd><?php print $node->field_contactdlr[0]['view'] ?></dd>
-</dl>
-<dl>
 <dt>Betreuung BALANCE</dt>
   <dd><?php print phptemplate_business_card($node->field_contactbalance[0]['uid']); ?></dd>
+<dt>Projekte</dt>
+  <dd><?php print $node->field_projects[0]['view']; ?></dd>
 </dl>
 <?php endif; ?>
