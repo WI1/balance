0cae98ececdfc09bb5fb66b1ef8bd4e356e7dfba
jakobstoeck <jakobstoeck>
2010-12-05 13:14:27 +0000

    the apache solr search result titles for user profiles are not linked correctly. I replace the link with the correct one with javascript until https://github.com/SupermanScott/apachesolr_users/issues/issue/1 is solved.

--- themes/balance/scripts.js
+++ themes/balance/scripts.js
@@ -12,42 +12,50 @@
  * $('label').labelOver('over-apply')
  */
 jQuery.fn.labelOver = function(overClass) {
-	return this.each(function(){
-		var label = jQuery(this);
-		var f = label.attr('for');
-		if (f) {
-			var input = jQuery('#' + f);
+  return this.each(function() {
+    var label = jQuery(this);
+    var f = label.attr('for');
+    if (f) {
+      var input = jQuery('#' + f);
 
-			this.hide = function() {
-			  label.css({ textIndent: -10000 })
-			}
+      this.hide = function() {
+        label.css({
+          textIndent: -10000
+        });
+      };
 
-			this.show = function() {
-			  if (input.val() == '') label.css({ textIndent: 0 })
-			}
+      this.show = function() {
+        if (input.val() == '') label.css({
+          textIndent: 0
+        });
+      };
 
-			// handlers
-			input.focus(this.hide);
-			input.blur(this.show);
+      // handlers
+      input.focus(this.hide);
+      input.blur(this.show);
 
-			label.addClass(overClass).click(function(){ input.focus() });
-			if (input.val() != '') this.hide();
-		}
-	})
-}
+      label.addClass(overClass).click(function() {
+        input.focus();
+      });
+      if (input.val() != '') this.hide();
+    }
+  });
+};
 
 $('label', '#user-login-form').labelOver('over-apply');
 $('label', '#edit-search-block-form-1-wrapper').labelOver('over-apply');
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('#block-nice_menus-1 li').hover(
-    function(){
+    function() {
       $('ul:first', $(this)).show();
     },
-    function(){
+    function() {
       $('ul', $(this)).hide();
     }
   );
-
+  // the apache solr search result titles for user profiles are not linked correctly. I replace the link with the correct one with javascript until https://github.com/SupermanScott/apachesolr_users/issues/issue/1 is solved.
+  $('dt.title a[href="http://' + window.location.host + '/"]', 'dl.apachesolr_search-results').attr('href', function() {
+    return $(this).parent().next().find('a').attr('href');
+  });
 });
-
