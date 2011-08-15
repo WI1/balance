45e100ae74303c7c2b418e885c7215197701302a
dominikboehler <dominikboehler>
2011-08-15 09:50:46 +0000

    embedded view

--- themes/balance/node-workshop.tpl.php
+++ themes/balance/node-workshop.tpl.php
@@ -1,5 +1,5 @@
 <?php if(arg(2) === null): ?>
-<?php print ''; ?>
+<?php print views_embed_view('og_ghp_ron'); ?>
 <?php elseif(arg(2) == 'info'): ?>
 <?php print $node->content['view']['#value']; ?>
 <?php endif; ?>
