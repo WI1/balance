094c7193340feb0af356f1f09da7ec2b61ed3571
dominikboehler <dominikboehler>
2011-08-15 09:48:53 +0000

    fixed view for aktuelles tab and info tab on non project groups

--- themes/balance/node-workshop.tpl.php
+++ themes/balance/node-workshop.tpl.php
@@ -1,5 +1,5 @@
 <?php if(arg(2) === null): ?>
-<?php print $node->content['view']['#value']; ?>
+<?php print ''; ?>
 <?php elseif(arg(2) == 'info'): ?>
-<?php print 'test'; ?>
+<?php print $node->content['view']['#value']; ?>
 <?php endif; ?>
